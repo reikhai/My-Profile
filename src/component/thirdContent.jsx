c81b24f240a2f2e01874f8b703f4b62a9cd25e45
reikhai <khai@chicken.com>
2026-04-10 16:40:56 +0800

    thirdcontent update

--- src/component/thirdContent.jsx
+++ src/component/thirdContent.jsx
@@ -18,17 +18,21 @@ import materialUI from "../images/material-ui-1.svg";
 import js from "../images/icons8-javascript.svg";
 
 import vmxBG from "../images/vmx_bg.png";
-import VMXLogo from "../images/vmx-logo.png";
-import foodbankBG from "../images/foodbank_bg.png";
-import foodbankLogo from "../images/food-bank-logo.png";
 import srBG from "../images/sr-bg.png";
 import srLogo from "../images/sr-logo.png";
+import mjLogo from "../images/mj-logo.png";
+import flutter from "../images/flutter.svg";
 import { motion } from "framer-motion";
 import Typography from "@mui/material/Typography";
+import { alpha } from "@mui/material/styles";
 
 const useStyles = makeStyles()((theme) => {
   const customeColor = theme.palette.primary.main;
   const bgColor = theme.palette.background.default;
+  const sectionGradient =
+    theme.palette.mode === "light"
+      ? `linear-gradient(147deg, ${alpha("#0f172a", 0.06)} 0%, ${bgColor} 74%)`
+      : `linear-gradient(147deg, #0a0d12 0%, ${bgColor} 74%)`;
 
   return {
     thirdContent: {
@@ -39,7 +43,7 @@ const useStyles = makeStyles()((theme) => {
         justifyContent: "center",
         width: "100%",
         zIndex: "1000",
-        background: `linear-gradient(147deg, #000000 0%, ${bgColor} 74%)`,
+        background: sectionGradient,
         padding: "40px 0px",
       },
       [theme.breakpoints.up("sm")]: {
@@ -54,7 +58,7 @@ const useStyles = makeStyles()((theme) => {
         width: "100%",
         zIndex: "1000",
         padding: "15px 30px",
-        background: `linear-gradient(147deg, #000000 0%, ${bgColor} 74%)`,
+        background: sectionGradient,
       },
     },
 
@@ -66,7 +70,7 @@ const useStyles = makeStyles()((theme) => {
       boxShadow: "none",
       borderWidth: "3px",
       borderStyle: "solid",
-      borderColor: "black",
+      borderColor: alpha(theme.palette.text.primary, 0.12),
       "&:hover": {
         boxShadow: `8px 8px 0px 0px ${customeColor}`,
         borderColor: `${customeColor}`,
@@ -153,54 +157,6 @@ export default function VerticalTabs() {
               </motion.div>
             </Grid>
 
-            {/* <Grid item xs={6} sm={4} md={4}>
-              <motion.div
-                className={classes.projectThumbnail}
-                initial="hidden"
-                whileInView="visible"
-                viewport={{ once: true }}
-                transition={{ duration: 1 }}
-                variants={{
-                  visible: { opacity: 1, scale: 1 },
-                  hidden: { opacity: 0, scale: 0 },
-                }}
-              >
-                <Card className={classes.projectFrame}>
-                  <Link
-                    href="http://foodbank.securelayers.cloud/admin"
-                    underline="none"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <CardActionArea>
-                      <CardMedia
-                        component="img"
-                        height="206"
-                        image={foodbankBG}
-                        alt="Foodbank"
-                      />
-                      <Box className={classes.projectTitle}>
-                        <img
-                          src={foodbankLogo}
-                          style={{
-                            maxWidth: "35px",
-                          }}
-                        />
-                      </Box>
-                    </CardActionArea>
-                  </Link>
-
-                  <CardActions sx={{ justifyContent: "center" }}>
-                    <Stack direction="row" spacing={4}>
-                      <img src={laravel} width={20} />
-                      <img src={reactjs} width={20} />
-                      <img src={materialUI} width={20} />
-                    </Stack>
-                  </CardActions>
-                </Card>
-              </motion.div>
-            </Grid> */}
-
             <Grid item xs={6} sm={4} md={4}>
               <motion.div
                 className={classes.projectThumbnail}
@@ -228,12 +184,6 @@ export default function VerticalTabs() {
                         alt="Demo"
                       />
                       <Box className={classes.projectTitle}>
-                        {/* <img
-                          src={VMXLogo}
-                          style={{
-                            maxWidth: "35px",
-                          }}
-                        /> */}
                         <Typography component={"span"} variant={"h4"}>
                           Portfolio
                         </Typography>
@@ -245,6 +195,44 @@ export default function VerticalTabs() {
                     <Stack direction="row" spacing={4}>
                       <img src={reactjs} width={20} />
                       <img src={materialUI} width={20} />
+                    </Stack>
+                  </CardActions>
+                </Card>
+              </motion.div>
+            </Grid>
+
+            <Grid item xs={6} sm={4} md={4}>
+              <motion.div
+                className={classes.projectThumbnail}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                transition={{ duration: 1 }}
+                variants={{
+                  visible: { opacity: 1, scale: 1 },
+                  hidden: { opacity: 0, scale: 0 },
+                }}
+              >
+                <Card className={classes.projectFrame}>
+                  <Link
+                    href="https://apps.apple.com/us/app/%E9%BA%BB%E5%B0%86%E8%BF%90%E5%8A%BF%E6%B5%8B%E7%AE%97/id6745542083"
+                    underline="none"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        height="206"
+                        image={mjLogo}
+                        alt="麻将运势测算（App Store）"
+                        sx={{ objectFit: "cover" }}
+                      />
+                    </CardActionArea>
+                  </Link>
+                  <CardActions sx={{ justifyContent: "center" }}>
+                    <Stack direction="row" spacing={4}>
+                      <img src={flutter} width={20} alt="Flutter" />
                     </Stack>
                   </CardActions>
                 </Card>
